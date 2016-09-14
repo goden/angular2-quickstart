54827a26be45df3b21bff352d2820aaff37518d2
Goden <goden.incredible@gmail.com>
2016-09-15 01:57:10 +0800

    Returned type is changed from array to promise object.

--- app/app.component.ts
+++ app/app.component.ts
@@ -86,6 +86,7 @@ export class AppComponent implements OnInit {
 	}
 
 	getHeroes(): void {
-		this.heroes = this.heroService.getHeroes();
+		// this.heroes = this.heroService.getHeroes();
+		this.heroService.getHeroes().then(heroes => this.heroes = heroes);
 	}
 }
