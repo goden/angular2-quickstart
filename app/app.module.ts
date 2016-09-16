6a68bda0670f909e6541dad31f5ee6d78b018c90
Goden <goden.incredible@gmail.com>
2016-09-17 03:18:22 +0800

    Use in-memory-data service to replace mock-heroes.

--- app/app.module.ts
+++ app/app.module.ts
@@ -3,6 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
+// Imports for loading and configuring the in-memory web api
+import { InMemoryWebApiModule } from 'angular2-in-memory-web-api';
+import { InMemoryDataService } from './in-memory-data.service';
+
 import { AppComponent } from './app.component';
 import { HeroDetailComponent } from './hero-detail.component';
 import { HeroesComponent } from './heroes.component';
@@ -16,7 +20,8 @@ import { routing } from './app.routing';
   					BrowserModule, 
   					FormsModule,
   					routing,
-  					HttpModule
+  					HttpModule,
+  					InMemoryWebApiModule.forRoot(InMemoryDataService)
   				],
   declarations: [ 
   					AppComponent, 
