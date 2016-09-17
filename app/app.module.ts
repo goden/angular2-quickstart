5767f7eb164e5941dd5fa2039e94cf468d0a491b
Goden <goden.incredible@gmail.com>
2016-09-18 04:03:51 +0800

    attach search tag to complete the search function.

--- app/app.module.ts
+++ app/app.module.ts
@@ -13,6 +13,7 @@ import { AppComponent } from './app.component';
 import { HeroDetailComponent } from './hero-detail.component';
 import { HeroesComponent } from './heroes.component';
 import { DashboardComponent } from './dashboard.component';
+import { HeroSearchComponent } from './hero-search.component';
 
 import { HeroService } from './hero.service';
 import { routing } from './app.routing';
@@ -29,7 +30,8 @@ import { routing } from './app.routing';
   					AppComponent, 
   					HeroDetailComponent, 
   					HeroesComponent,
-  					DashboardComponent
+  					DashboardComponent,
+  					HeroSearchComponent
   				],
   bootstrap: 	[ 
   					AppComponent 
