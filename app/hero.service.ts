e49ca158536b72940b3ae062168f24717b43c311
Goden <goden.incredible@gmail.com>
2016-09-18 01:12:22 +0800

    add function to update the hero name by using http PUT method.

--- app/hero.service.ts
+++ app/hero.service.ts
@@ -11,6 +11,9 @@ import { Hero } from './hero';
 export class HeroService {
 
 	private heroesUrl = 'app/heroes';
+	private headers = new Headers({
+		'Content-Type': 'application/json'
+	});
 
 	constructor(private http: Http) {}
 
@@ -26,6 +29,16 @@ export class HeroService {
 	getHero(id: number): Promise<Hero> {
 		return this.getHeroes()
 					.then(heroes => heroes.find(hero => hero.id === id));
+	}
+
+	update(hero: Hero): Promise<Hero> {
+		const url = `${this.heroesUrl}/${hero.id}`;
+
+		return this.http
+					.put(url, JSON.stringify(hero), {headers: this.headers})
+					.toPromise()
+					.then(() => hero)
+					.catch(this.handleError);
 	}
 
 
